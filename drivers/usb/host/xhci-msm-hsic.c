9a619f2c402974e93a87a7813975565fa80fc0d7
imoseyon <imoseyon@gmail.com>
2015-02-14 21:52:34 -0800

    hsic: allow wl_divide = 0 to completely disable hsic wake source
    Change-Id: I448c3a4bdfb99903b45728b2d2d0f17d5b4c516c

--- drivers/usb/host/xhci-msm-hsic.c
+++ drivers/usb/host/xhci-msm-hsic.c
@@ -691,7 +691,9 @@ static irqreturn_t mxhci_hsic_wakeup_irq(int irq, void *data)
 	xhci_dbg_log_event(&dbg_hsic, NULL, "Remote Wakeup IRQ",
 			mxhci->wakeup_int_cnt);
 
-	pm_stay_awake(mxhci->dev);
+	if (wl_divide == 1) pm_stay_awake(mxhci->dev);
+	else if (wl_divide > 1) pm_wakeup_event(mxhci->dev, 
+		WL_TIMEOUT/wl_divide);
 
 	spin_lock(&mxhci->wakeup_lock);
 	if (mxhci->wakeup_irq_enabled) {
@@ -925,7 +927,7 @@ static int mxhci_hsic_suspend(struct mxhci_hsic_hcd *mxhci)
 	/* disable force-on mode for periph_on */
 	clk_set_flags(mxhci->system_clk, CLKFLAG_NORETAIN_PERIPH);
 
-	pm_relax(mxhci->dev);
+	if (wl_divide > 0) pm_relax(mxhci->dev);
 
 	dev_dbg(mxhci->dev, "HSIC-USB in low power mode\n");
 	xhci_dbg_log_event(&dbg_hsic, NULL, "Controller suspended", 0);
@@ -944,7 +946,9 @@ static int mxhci_hsic_resume(struct mxhci_hsic_hcd *mxhci)
 		return 0;
 	}
 
-	pm_stay_awake(mxhci->dev);
+	if (wl_divide == 1) pm_stay_awake(mxhci->dev);
+	else if (wl_divide > 1) pm_wakeup_event(mxhci->dev, 
+		WL_TIMEOUT/wl_divide);
 
 	/* enable force-on mode for periph_on */
 	clk_set_flags(mxhci->system_clk, CLKFLAG_RETAIN_PERIPH);
@@ -1530,7 +1534,10 @@ static int mxhci_hsic_probe(struct platform_device *pdev)
 	init_waitqueue_head(&mxhci->phy_in_lpm_wq);
 
 	device_init_wakeup(&pdev->dev, 1);
-	pm_stay_awake(mxhci->dev);
+
+	if (wl_divide == 1) pm_stay_awake(mxhci->dev);
+	else if (wl_divide > 1) pm_wakeup_event(mxhci->dev, 
+		WL_TIMEOUT/wl_divide);
 
 	pm_runtime_set_active(&pdev->dev);
 	pm_runtime_enable(&pdev->dev);
