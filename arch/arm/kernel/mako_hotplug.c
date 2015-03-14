8b08d52b89e8a48f7bc3bd232f5ad20607e67bd2
franciscofranco <franciscofranco.1990@gmail.com>
2015-03-14 03:02:23 +0000

    mako_hotplug: don't use PAGE_SIZE for snprintf
    Change-Id: Icef7154330d8238d9b3522fd61dab775bc5bbfc6
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/kernel/mako_hotplug.c
+++ arch/arm/kernel/mako_hotplug.c
@@ -255,7 +255,7 @@ static ssize_t load_threshold_show(struct device *dev,
 {
 	struct hotplug_tunables *t = &tunables;
 
-	return snprintf(buf, PAGE_SIZE, "%u\n", t->load_threshold);
+	return snprintf(buf, 10, "%u\n", t->load_threshold);
 }
 
 static ssize_t load_threshold_store(struct device *dev,
@@ -279,7 +279,7 @@ static ssize_t high_load_counter_show(struct device *dev,
 {
 	struct hotplug_tunables *t = &tunables;
 
-	return snprintf(buf, PAGE_SIZE, "%u\n", t->high_load_counter);
+	return snprintf(buf, 10, "%u\n", t->high_load_counter);
 }
 
 static ssize_t high_load_counter_store(struct device *dev,
@@ -303,7 +303,7 @@ static ssize_t max_load_counter_show(struct device *dev,
 {
 	struct hotplug_tunables *t = &tunables;
 
-	return snprintf(buf, PAGE_SIZE, "%u\n", t->max_load_counter);
+	return snprintf(buf, 10, "%u\n", t->max_load_counter);
 }
 
 static ssize_t max_load_counter_store(struct device *dev,
@@ -327,7 +327,7 @@ static ssize_t cpufreq_unplug_limit_show(struct device *dev,
 {
 	struct hotplug_tunables *t = &tunables;
 
-	return snprintf(buf, PAGE_SIZE, "%u\n", t->cpufreq_unplug_limit);
+	return snprintf(buf, 10, "%u\n", t->cpufreq_unplug_limit);
 }
 
 static ssize_t cpufreq_unplug_limit_store(struct device *dev,
@@ -351,7 +351,7 @@ static ssize_t min_time_cpu_online_show(struct device *dev,
 {
 	struct hotplug_tunables *t = &tunables;
 
-	return snprintf(buf, PAGE_SIZE, "%u\n", t->min_time_cpu_online);
+	return snprintf(buf, 10, "%u\n", t->min_time_cpu_online);
 }
 
 static ssize_t min_time_cpu_online_store(struct device *dev,
@@ -375,7 +375,7 @@ static ssize_t timer_show(struct device *dev, struct device_attribute *attr,
 {
 	struct hotplug_tunables *t = &tunables;
 
-	return snprintf(buf, PAGE_SIZE, "%u\n", t->timer);
+	return snprintf(buf, 10, "%u\n", t->timer);
 }
 
 static ssize_t timer_store(struct device *dev, struct device_attribute *attr,
