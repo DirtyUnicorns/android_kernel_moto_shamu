a1e4051c4510eda7820144b25076456f96343ef2
Pranav Vashi <neobuddy89@gmail.com>
2015-05-23 03:31:25 +0530

    cpufreq: impulse: Replace obsolete strict_strtoul calls
    strict_strtoul has been replaced by kstrtoul.
    
    Change-Id: I40bb863ab9bd0997a5bb58858a34b80d21a997a1
    Signed-off-by: Bryan Freed <bfreed@chromium.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/cpufreq/cpufreq_impulse.c
+++ drivers/cpufreq/cpufreq_impulse.c
@@ -912,7 +912,7 @@ static ssize_t store_hispeed_freq(struct cpufreq_impulse_tunables *tunables,
 	int ret;
 	long unsigned int val;
 
-	ret = strict_strtoul(buf, 0, &val);
+	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
 	tunables->hispeed_freq = val;
@@ -953,7 +953,7 @@ static ssize_t store_go_hispeed_load(struct cpufreq_impulse_tunables
 	int ret;
 	unsigned long val;
 
-	ret = strict_strtoul(buf, 0, &val);
+	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
 	tunables->go_hispeed_load = val;
@@ -972,7 +972,7 @@ static ssize_t store_go_lowspeed_load(struct cpufreq_impulse_tunables
 	int ret;
 	unsigned long val;
 
-	ret = strict_strtoul(buf, 0, &val);
+	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
 	tunables->go_lowspeed_load = val;
@@ -991,7 +991,7 @@ static ssize_t store_min_sample_time(struct cpufreq_impulse_tunables
 	int ret;
 	unsigned long val;
 
-	ret = strict_strtoul(buf, 0, &val);
+	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
 	tunables->min_sample_time = val;
@@ -1010,7 +1010,7 @@ static ssize_t store_timer_rate(struct cpufreq_impulse_tunables *tunables,
 	int ret;
 	unsigned long val, val_round;
 
-	ret = strict_strtoul(buf, 0, &val);
+	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
 
