06d31e67ef0fe1cc5ed1959c059c8db0bb779bd5
buckmarble <forrestmarble@gmail.com>
2015-08-08 15:21:58 -0400

    battery: fastcharge update
    Change-Id: I1a600747d141f2a6f1f6e79ec6ba373d99808b04

--- drivers/power/smb135x-charger.c
+++ drivers/power/smb135x-charger.c
@@ -1539,17 +1539,14 @@ static int smb135x_set_usb_chg_current(struct smb135x_chg *chip,
 		goto out;
 	}
 	if (current_ma == CURRENT_500_MA) {
+
 #ifdef CONFIG_FORCE_FAST_CHARGE
-		if (force_fast_charge) {
-			current_ma = CURRENT_900_MA;
-			rc = smb135x_masked_write(chip, CFG_5_REG,
-				USB_2_3_BIT, USB_2_3_BIT);
-		} else
-			rc = smb135x_masked_write(chip, CFG_5_REG,
-				USB_2_3_BIT, 0);
-#else
-		rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, 0);
+		if (force_fast_charge)
+			rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, USB_2_3_BIT);
+		else
 #endif
+			rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, 0);
+	
 		rc |= smb135x_masked_write(chip, CMD_INPUT_LIMIT,
 				USB_100_500_AC_MASK, USB_500_VAL);
 		rc |= smb135x_path_suspend(chip, USB, CURRENT, false);
