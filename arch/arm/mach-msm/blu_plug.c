d7c2f21c22716094934757a70f518185b500069f
Joe Schubert <joseph.schubert@gmail.com>
2015-10-27 17:23:26 -0400

    Blu_Plug: Enable/disable by sysfs
    Change-Id: I83d708f72a48cc5cfe6945854620b4016ed9bb5a

--- arch/arm/mach-msm/blu_plug.c
+++ arch/arm/mach-msm/blu_plug.c
@@ -38,8 +38,11 @@
 #define MAX_FREQ_SCREENOFF (1190400)
 #define MAX_FREQ_PLUG (2649600)
 #define DEF_PLUG_THRESHOLD 0
+#define BLU_PLUG_ENABLED	0
 
-static unsigned int up_threshold = UP_THRESHOLD;;
+static unsigned int blu_plug_enabled = BLU_PLUG_ENABLED;
+
+static unsigned int up_threshold = UP_THRESHOLD;
 static unsigned int delay = DELAY;
 static unsigned int min_online = MIN_ONLINE;
 static unsigned int max_online = MAX_ONLINE;
@@ -452,8 +455,11 @@ module_param_array(plug_threshold, uint, NULL, 0644);
 
 /***************** end of module parameters *****************/
 
-static int __init dyn_hp_init(void)
+static int dyn_hp_init(void)
 {
+	if (!blu_plug_enabled) {
+		return 0;
+	}
 	notify.notifier_call = fb_notifier_callback;
 	if (fb_register_client(&notify) != 0)
 		pr_info("%s: Failed to register FB notifier callback\n", __func__);
@@ -472,7 +478,7 @@ static int __init dyn_hp_init(void)
 	return 0;
 }
 
-static void __exit dyn_hp_exit(void)
+static void dyn_hp_exit(void)
 {
 	cancel_delayed_work_sync(&dyn_work);
 	fb_unregister_client(&notify);
@@ -480,6 +486,36 @@ static void __exit dyn_hp_exit(void)
 	
 	pr_info("%s: deactivated\n", __func__);
 }
+
+
+/* enabled */
+static int set_enabled(const char *val, const struct kernel_param *kp)
+{
+	int ret = 0;
+	unsigned int i;
+	int blu = 0;
+
+	ret = kstrtouint(val, 10, &i);
+	if (ret)
+		return -EINVAL;
+	if (i < 0 || i > 1)
+		return 0;
+
+	ret = param_set_uint(val, kp);
+	blu_plug_enabled = i;
+	if ((blu_plug_enabled == 1))
+		blu = dyn_hp_init();
+	if ((blu_plug_enabled == 0))
+		dyn_hp_exit();
+	return i;
+}
+
+static struct kernel_param_ops enabled_ops = {
+	.set = set_enabled,
+	.get = param_get_uint,
+};
+
+module_param_cb(enabled, &enabled_ops, &blu_plug_enabled, 0644);
 
 MODULE_AUTHOR("Stratos Karafotis <stratosk@semaphore.gr");
 MODULE_AUTHOR("engstk <eng.stk@sapo.pt>");
