ac84c9df429f8e2fe5274c1c2dc5a34f35b5d4e4
Pranav Vashi <neobuddy89@gmail.com>
2016-04-10 11:47:48 +0530

    state_notifier: schedule work on any available CPU
    Change-Id: I76b344fe20c4cad5c9f7501588ce1944180acdaf
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/input/touchscreen/state_notifier.c
+++ drivers/input/touchscreen/state_notifier.c
@@ -1,7 +1,7 @@
 /*
  * State Notifier Driver
  *
- * Copyright (c) 2013-2015, Pranav Vashi <neobuddy89@gmail.com>
+ * Copyright (c) 2013-2016, Pranav Vashi <neobuddy89@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -101,7 +101,7 @@ void state_suspend(void)
 
 	suspend_in_progress = true;
 
-	queue_delayed_work_on(0, susp_wq, &suspend_work, 
+	queue_delayed_work(susp_wq, &suspend_work, 
 		msecs_to_jiffies(suspend_defer_time * 1000));
 }
 
@@ -112,7 +112,7 @@ void state_resume(void)
 	suspend_in_progress = false;
 
 	if (state_suspended)
-		queue_work_on(0, susp_wq, &resume_work);
+		queue_work(susp_wq, &resume_work);
 }
 
 static int fb_notifier_callback(struct notifier_block *self,
