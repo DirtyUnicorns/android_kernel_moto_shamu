0e02e3d1f9e8ac21e256e6f447b3d3b77d14ba28
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-08-23 17:50:54 -0400

    Shamu : Lionfish Governor
    Change-Id: I4eeb9d525c3fcb8568a5090b730d4c9aa5d71eac
    Signed-off-by: moepda <moepda@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -429,6 +429,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONFISH)
+extern struct cpufreq_governor cpufreq_gov_lionfish;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lionfish)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
