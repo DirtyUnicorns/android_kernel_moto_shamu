c73495f9d31bfa8ab9b44f2d65855f696d2846fc
Steven Barrett <steven@liquorix.net>
2017-02-21 03:44:46 -0800

    zen-tune: implement zen-tune v4.10
    4.9:
    In a surprising turn of events, while benchmarking and testing
    hierarchical scheduling with BFQ + writeback throttling, it turns out
    that raising the number of requests in queue _actually_ improves
    responsiveness and completely eliminates the random stalls that would
    normally occur without hierarchical scheduling.
    
    To make this test more intense, I used the following test:
    
    Rotational disk1: rsync -a /source/of/data /target/to/disk1
    Rotational disk2: rsync -a /source/of/data /target/to/disk2
    
    And periodically attempted to write super fast with:
    dd if=/dev/zero of=/target/to/disk1/block bs=4096
    
    This wrote 10gb incredibly fast to writeback and I encountered zero
    stalls through this entire test of 10-15 minutes.
    
    My suspicion is that with cgroups, BFQ is more able to properly sort
    among multiple drives, reducing the chance of a starved process.  This
    plus writeback throttling completely eliminate any outstanding bugs with
    high writeback ratios, letting the user enjoy low latency writes
    (application thinks they're already done), and super high throughput due
    to batched writes in writeback.
    
    Please note however, without the following configuration, I cannot
    guarantee you will not get stalls:
    
    CONFIG_BLK_CGROUP=y
    CONFIG_CGROUP_WRITEBACK=y
    CONFIG_IOSCHED_CFQ=y
    CONFIG_CFQ_GROUP_IOSCHED=y
    CONFIG_IOSCHED_BFQ=y
    CONFIG_BFQ_GROUP_IOSCHED=y
    CONFIG_DEFAULT_BFQ=y
    CONFIG_SCSI_MQ_DEFAULT=n
    
    Special thanks to h2, author of smxi and inxi, for providing evidence
    that a configuration specific to Debian did not cause stalls found the
    Liquorix kernels under heavy IO load.  This specific configuration
    turned out to be hierarchical scheduling on CFQ (thus, BFQ as well).
    
    4.10:
    During some personal testing with the Dolphin emulator, MuQSS has
    serious problems scaling its frequencies causing poor performance where
    boosting the CPU frequencies would have fixed them.  Reducing the
    up_threshold to 45 with MuQSS appears to fix the issue, letting the
    introduction to "Star Wars: Rogue Leader" run at 100% speed versus about
    80% on my test system.
    
    Also, lets refactor the definitions and include some indentation to help
    the reader discern what the scope of all the macros are.
    
    Change-Id: I8917da316707c22ac2f5b0535691edbc98fbc6a3
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -73,7 +73,7 @@ static long ratelimit_pages = 32;
 #ifdef CONFIG_ZEN_INTERACTIVE
 int dirty_background_ratio = 20;
 #else
-int dirty_background_ratio = 25;
+int dirty_background_ratio = 10;
 #endif
 
 /*
@@ -92,9 +92,9 @@ int vm_highmem_is_dirtyable;
  * The generator of dirty data starts writeback at this percentage
  */
 #ifdef CONFIG_ZEN_INTERACTIVE
-int vm_dirty_ratio = 30;
-#else
 int vm_dirty_ratio = 50;
+#else
+int vm_dirty_ratio = 20;
 #endif
 
 /*
