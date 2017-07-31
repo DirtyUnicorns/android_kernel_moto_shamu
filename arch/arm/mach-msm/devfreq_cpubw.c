CAF-staging/msm8916_kernel

PM / devfreq: Revert addition of devfreq_governor_data and related changes
This reverts commit 0e5b3352b5f04919a250bba695444c3a29672525 and fixes
other code that resulted from it. The devfreq_governor_data field is not
needed since the governors can use other means (like container_of) to find
out device specific private data

Change-Id: Ib7e5db29b40d9676bbb5cf6057e0072665ace821
Signed-off-by: Saravana Kannan <skannan@codeaurora.org>

drivers/devfreq/devfreq_cpubw.c
@@ -108,19 +108,9 @@ int cpubw_target(struct device *dev, unsigned long *freq, u32 flags)
 	return set_bw(*freq, gov_ab);
 }
 
-static struct devfreq_governor_data gov_data[] = {
-	{ .name = "performance" },
-	{ .name = "powersave" },
-	{ .name = "userspace" },
-	{ .name = "cpufreq" },
-	{ .name = "cpubw_hwmon", .data = &gov_ab },
-};
-
 struct devfreq_dev_profile cpubw_profile = {
 	.polling_ms = 50,
 	.target = cpubw_target,
-	.governor_data = gov_data,
-	.num_governor_data = ARRAY_SIZE(gov_data),
 };
 
 #define PROP_PORTS "qcom,cpu-mem-ports"
@@ -188,7 +178,7 @@ static int __init cpubw_probe(struct platform_device *pdev)
 		return -ENODEV;
 	}
 
-	df = devfreq_add_device(dev, &cpubw_profile, "cpufreq", NULL);
+	df = devfreq_add_device(dev, &cpubw_profile, "cpufreq", &gov_ab);
 	if (IS_ERR(df)) {
 		msm_bus_scale_unregister_client(bus_client);
 		return PTR_ERR(df);

DirtyUnicorns/android_kernel_moto_shamu

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

arch/arm/mach-msm/devfreq_cpubw.c
@@ -108,19 +108,9 @@ int cpubw_target(struct device *dev, unsigned long *freq, u32 flags)
 	return set_bw(*freq, gov_ab);
 }
 
-static struct devfreq_governor_data gov_data[] = {
-	{ .name = "performance" },
-	{ .name = "powersave" },
-	{ .name = "userspace" },
-	{ .name = "msm_cpufreq" },
-	{ .name = "cpubw_hwmon", .data = &gov_ab },
-};
-
 struct devfreq_dev_profile cpubw_profile = {
 	.polling_ms = 50,
 	.target = cpubw_target,
-	.governor_data = gov_data,
-	.num_governor_data = ARRAY_SIZE(gov_data),
 };
 
 #define PROP_PORTS "qcom,cpu-mem-ports"
@@ -188,7 +178,7 @@ static int __init cpubw_probe(struct platform_device *pdev)
 		return -ENODEV;
 	}
 
-	df = devfreq_add_device(dev, &cpubw_profile, "cpubw_hwmon", NULL);
+	df = devfreq_add_device(dev, &cpubw_profile, "cpubw_hwmon", &gov_ab);
 	if (IS_ERR(df)) {
 		msm_bus_scale_unregister_client(bus_client);
 		return PTR_ERR(df);