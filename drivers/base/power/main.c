e5e6b73dd7c58a7b002fb6e942011c28223f6fb3
matthewdalex <matthewalex@outlook.com>
2017-11-13 16:22:55 -0500

    power: use print_active_wakeup_sources for wakelock blocking

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -594,7 +594,7 @@ static void dpm_resume_early(pm_message_t state)
 	ktime_t starttime = ktime_get();
 
 #ifdef CONFIG_BOEFFLA_WL_BLOCKER
-	pm_print_active_wakeup_sources();
+	print_active_wakeup_sources();
 #endif
 
 	mutex_lock(&dpm_list_mtx);
